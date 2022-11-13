7f2dceff2c5493834834dd73e2e338459134e60b
ZhengRyan <ryan.zhengrp@gmail.com>
2022-11-13 23:29:42 +0800

    update requirements.txt

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ZhengRyan/autotreemodel',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
